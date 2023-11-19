d364ff86f249500db449b790f4ae41f40e385c49
Gersuer Henrique <gersuerhenrique@hotmail.com>
2023-11-19 16:25:36 -0300

    Update #CAESAR CYPHER.py

--- #CAESAR CYPHER.py
+++ #CAESAR CYPHER.py
@@ -52,3 +52,33 @@ while True:
     break
 
   
+#Doing the same thing with one function
+
+def casear_cipher(message, shift, direction):
+  if direction == 'decode':
+      shift *= -1
+  initial_message = ''
+  for letter in message:
+    position = alphabet.index(letter)
+    new_position = position + shift
+    new_letter = alphabet[new_position]
+    initial_message += new_letter
+  print(f"The {direction} is {initial_message}")
+
+while True:
+  direction = input("What you want to do? Encode or Decode:\n").lower()
+  message = input("What is your message?\n").lower()
+  shift = int(input("Type the shift number: "))
+  if direction == '' or (direction != 'decode' and direction != 'encode') or shift == '':
+    print("Type if you want to decode or endode your message, your message and how many shifts you want.")
+  else:
+    casear_cipher(message, shift, direction)
+  while True:
+    go_out = input("Do you want to exit? Y/N \n").lower()
+    if go_out != 'y' and go_out!='n':
+      print("Please respond the answer with Y or N")
+    if go_out == 'y' or go_out == 'n':
+      break
+  if go_out == 'y':
+    print("Thanks for used our app.")
+    break 
